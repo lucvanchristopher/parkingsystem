05400dcf43be3a53c1550ccf3c40fc269ca53de9
lucvanchristopher <lucvanchristopher@gmail.com>
2021-02-27 23:02:04 +0100

    feat: allow discount in fare computing

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,7 +5,16 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket) {
+    /**
+     * Calculate the parking fare
+     *
+     * @param ticket   {{@link Ticket}} The ticket from which we should compute the fare
+     * @param discount {double} a discount rate
+     */
+    public void calculateFare(Ticket ticket, double discount) {
+        if (discount > 1 || discount < 0) {
+            throw new IllegalArgumentException("Invalid discount rate");
+        }
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
@@ -23,17 +32,30 @@ public class FareCalculatorService {
         } else {
             duration = Math.round(duration / 60);
         }
+        double perHourPrice = 0;
         switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                perHourPrice = Fare.CAR_RATE_PER_HOUR;
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                perHourPrice = Fare.BIKE_RATE_PER_HOUR;
                 break;
             }
-            default:
-                throw new IllegalArgumentException("Unkown Parking Type");
+            default: throw new IllegalArgumentException("Unknown Parking Type");
         }
+
+        double price = duration * perHourPrice * (1 - discount);
+        ticket.setPrice(price);
+    }
+
+    /**
+     * Calculate the parking fare
+     *
+     * @param ticket {{@link Ticket}} The ticket from which we should compute the fare
+     */
+    public void calculateFare(Ticket ticket) {
+        // Call the above function with a default discount of 0 (no discount)
+        this.calculateFare(ticket, 0);
     }
 }
